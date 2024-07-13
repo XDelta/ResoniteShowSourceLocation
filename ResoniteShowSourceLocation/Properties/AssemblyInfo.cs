4525617c20cf953ed5f27f8216adf82d63105764
Delta <delta@deltawolf.net>
2024-07-13 08:29:59 -0700

    Update Assembly Info

--- ResoniteShowSourceLocation/Properties/AssemblyInfo.cs
+++ ResoniteShowSourceLocation/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using ShowSourceLocation;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ResoniteShowSourceLocation")]
+[assembly: AssemblyTitle("ShowSourceLocation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Delta")]
-[assembly: AssemblyProduct("ResoniteShowSourceLocation")]
-[assembly: AssemblyCopyright("Copyright © Delta 2023")]
+[assembly: AssemblyProduct("ShowSourceLocation")]
+[assembly: AssemblyCopyright("Copyright © Delta 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1")]
-[assembly: AssemblyFileVersion("1.1.1")]
+[assembly: AssemblyVersion(ResoniteShowSourceLocation.VERSION_CONSTANT)]
+[assembly: AssemblyFileVersion(ResoniteShowSourceLocation.VERSION_CONSTANT)]
